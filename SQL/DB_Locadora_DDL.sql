080ae5412843055d87a16e17bc80edaa5ee1a970
André Akira Kusaba <43036207856@info.local>
2020-02-03 11:40:57 -0200

    Aula 6

---
+++ SQL/DB_Locadora_DDL.sql
@@ -0,0 +1,51 @@
+-- DDL
+
+CREATE DATABASE DB_Locadora;
+
+
+DROP DATABASE DB_Locadora;
+USE DB_Locadora;
+
+CREATE TABLE Tabela_Empresa (
+	IdEmpresa   INT PRIMARY KEY IDENTITY,
+	NomeEmpresa VARCHAR (200) NOT NULL
+);
+
+CREATE TABLE Tabela_Marca (
+	IdMarca     INT PRIMARY KEY IDENTITY,
+	NomeMarca   VARCHAR (200)
+);
+
+CREATE TABLE Tabela_Cliente (
+	IdCliente   INT PRIMARY KEY IDENTITY,
+	NomeCliente VARCHAR (200),
+	Cpf         VARCHAR (200)
+);
+
+CREATE TABLE Tabela_Modelo (
+	IdModelo   INT PRIMARY KEY IDENTITY,
+	NomeModelo VARCHAR (200),
+	IdMarca    INT FOREIGN KEY REFERENCES Tabela_Marca(IdMarca)
+);
+
+CREATE TABLE Tabela_Veiculo (
+	IdVeiculo  INT PRIMARY KEY IDENTITY,
+	NumPlaca   VARCHAR (200),
+	IdEmpresa  INT FOREIGN KEY REFERENCES Tabela_Empresa(IdEmpresa),
+	IdModelo   INT FOREIGN KEY REFERENCES Tabela_Modelo(IdModelo)
+);
+
+CREATE TABLE Tabela_Aluguel (
+	IdAluguel  INT PRIMARY KEY IDENTITY,
+	DataInicio DATE,
+	DataFim    DATE,
+	IdCliente  INT FOREIGN KEY REFERENCES Tabela_Cliente(IdCliente),
+	IdVeiculo  INT FOREIGN KEY REFERENCES Tabela_Veiculo(IdVeiculo)
+);
+
+SELECT * FROM Tabela_Aluguel;
+SELECT * FROM Tabela_Cliente;
+SELECT * FROM Tabela_Empresa;
+SELECT * FROM Tabela_Marca;
+SELECT * FROM Tabela_Modelo;
+SELECT * FROM Tabela_Veiculo;
--- SQL/SQLQuery4.sql
+++ SQL/SQLQuery4.sql
@@ -6,46 +6,54 @@ CREATE DATABASE DB_Optus;
 USE DB_Optus;
 
 CREATE TABLE Tabela_estilos(
-	IdEstilos INT PRIMARY KEY IDENTITY,
-	Estilo_Musical VARCHAR (200)
+	IdEstilos         INT PRIMARY KEY IDENTITY,
+	Estilo_Musical    VARCHAR (200)
 );
 
 CREATE TABLE Tabela_artistas(
-	IdArtista INT PRIMARY KEY IDENTITY,
-	Nome_Artistas VARCHAR (200)
+	IdArtista         INT PRIMARY KEY IDENTITY,
+	Nome_Artistas     VARCHAR (200)
 );
 
 CREATE TABLE Tabela_albuns(
-	IdAlbum INT PRIMARY KEY IDENTITY,
-	NomeAlbum VARCHAR (200),
-	DataLancamento DATE,
-	Localizacao VARCHAR (200),
+	IdAlbum           INT PRIMARY KEY IDENTITY,
+	NomeAlbum         VARCHAR (200),
+	DataLancamento    DATE,
+	Localizacao       VARCHAR (200),
 	QuantidadeMinutos BIGINT,
-	IdArtista INT FOREIGN KEY REFERENCES Tabela_artistas (IdArtista),
-	IdEstilo INT FOREIGN KEY REFERENCES Tabela_estilos (IdEstilos)
+	IdArtista         INT FOREIGN KEY REFERENCES Tabela_artistas (IdArtista),
+	IdEstilo          INT FOREIGN KEY REFERENCES Tabela_estilos (IdEstilos)
 );
 
 CREATE TABLE Tabela_usuario(
-	IdUsuario INT PRIMARY KEY IDENTITY,
-	TipoUsuario VARCHAR (200),
-	NomeUsuario VARCHAR (200),
+	IdUsuario         INT PRIMARY KEY IDENTITY,
+	TipoUsuario       VARCHAR (200),
+	NomeUsuario       VARCHAR (200),
 );
 
 INSERT INTO Tabela_estilos(Estilo_Musical)
-VALUES ('Samba'), ('Rock'), ('MPB'), ('Blues'), ('Eletronica');
+VALUES  ('Samba'), 
+		('Rock'), 
+		('MPB'), 
+		('Blues'), 
+		('Eletronica');
 
 INSERT INTO Tabela_artistas(Nome_Artistas)
 VALUES ('Toquinho'), ('Raul Seixas'), ('Djavan'), ('Frank Sinatra'), ('David Gueta');
 
-INSERT INTO Tabela_albuns(NomeAlbum, DataLancamento, Localizacao, QuantidadeMinutos)
-VALUES ('Whisky na praia', '2000-02-20', 'SP', '1'), 
-	('Gita', '2000-02-20', 'SP', '1'), 
-	('Samurai', '2000-02-20', 'SP', '1'), 
-	('Fly me to the moon', '2000-02-20', 'SP', '1'), 
-	('Love is gonne', '2000-02-20', 'SP', '1');
+INSERT INTO Tabela_albuns(NomeAlbum, DataLancamento, Localizacao, QuantidadeMinutos, IdArtista, IdEstilo)
+VALUES ('Whisky na praia', '2000-02-20', 'SP', '1', '2', '1'), 
+	   ('Gita', '2000-02-20', 'SP', '1', '2', '1'), 
+	   ('Samurai', '2000-02-20', 'SP', '1', '2', '1'), 
+	   ('Fly me to the moon', '2000-02-20', 'SP', '1', '2', '1'), 
+	   ('Love is gonne', '2000-02-20', 'SP', '1', '2', '1');
 
 INSERT INTO Tabela_usuario(TipoUsuario, NomeUsuario)
-VALUES ('ADM', 'Carol'), ('ADM', 'Saulo'), ('User', 'Seila'), ('User', 'Eita'), ('User', 'Criatividades1000');
+VALUES  ('ADM', 'Carol'), 
+		('ADM', 'Saulo'), 
+		('User', 'Seila'), 
+		('User', 'Eita'), 
+		('User', 'Criatividades1000');
 
 UPDATE Tabela_artistas
 SET Nome_Artistas = 'Vinicius e Toquinho'
@@ -59,8 +67,38 @@ UPDATE Tabela_usuario
 SET TipoUsuario = 'User'
 WHERE IdUsuario = 1;
 
-SELECT * FROM Tabela_estilos;
-SELECT * FROM Tabela_artistas;
-SELECT * FROM Tabela_albuns;
-SELECT * FROM Tabela_usuario;
+--  COM INNER JOIN 
 
+SELECT * FROM Tabela_artistas
+INNER JOIN Tabela_albuns ON Tabela_artistas.IdArtista = Tabela_albuns.IdAlbum
+WHERE Tabela_albuns.IdArtista = 2;
+
+-- SELECIONAR OS ALBUNS LAÇAR NA MESMA DATA
+
+SELECT * FROM Tabela_albuns WHERE DataLancamento = '2010-02-03';
+
+
+-- COM INNER JOIN
+
+SELECT * FROM Tabela_artistas
+INNER JOIN Tabela_albuns ON Tabela_artistas.IdArtista = Tabela_albuns.IdArtista
+WHERE DataLancamento = '2010-02-03';
+
+
+-- SELECIONAR ALBUNS E ARTISTAS E ORDENAR POR DATA DE LANCAMENTO
+
+SELECT * FROM Tabela_albuns ORDER BY DataLancamento DESC;
+
+
+-- COM INNER JOIN 
+
+SELECT * FROM Tabela_albuns
+INNER JOIN Tabela_artistas ON Tabela_albuns.IdArtista = Tabela_artistas.IdArtista
+ORDER BY DataLancamento DESC;
+
+-- SELECIONAR OS ARTISTAS DO MESMO ESTILO MUSICAL
+
+SELECT Tabela_artistas.Nome_Artistas, Tabela_estilos.Estilo_Musical FROM Tabela_artistas
+INNER JOIN Tabela_albuns ON Tabela_artistas.IdArtista = Tabela_albuns.IdAlbum
+INNER JOIN Tabela_estilos ON Tabela_artistas.IdArtista = Tabela_estilos.IdEstilos
+WHERE Tabela_estilos.IdEstilos = 2;
